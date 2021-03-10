71bb74f8510857622b87d2f0d76b2a7818191095
Черепахин Василий Геннадьевич <vasi.che@gmail.com>
2021-03-10 11:17:17 +0500

    Заготовка

---
+++ src/main/java/ru/perm/v/integrtest/IntegrtestApplication.java
@@ -0,0 +1,13 @@
+package ru.perm.v.integrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/perm/v/integrtest/IntegrtestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.perm.v.integrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
